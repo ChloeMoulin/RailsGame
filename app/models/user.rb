75b441865a459083a2fcb7cf780189bcb8d016ea
ChloeMoulin <chloe.moulin42@free.fr>
2015-09-07 15:59:17 +0200

    Users + sessions

---
+++ app/models/user.rb
@@ -0,0 +1,41 @@
+require 'digest'
+class User < ActiveRecord::Base
+  attr_accessible :email, :hashed_password, :role, :username, :password, :password_confirmation
+  attr_accessor :password, :password_confirmation
+  validates :email, :uniqueness => true, 
+  					:length => {:within => 5..50},
+  					:format => {:with => /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$/i}
+
+  validates :username, :uniqueness => true
+
+  validates :password, :length => {:within => 10..50},
+  						:confirmation => true,
+  						:presence => true,
+  						:if => :password_required?
+
+before_save :encrypt_new_password
+
+def self.authenticate(email, password)
+	user = find_by_email(email)
+	return user if user && user.authenticated?(password)
+end
+
+def authenticated?(password)
+	self.hashed_password == encrypt(password)
+end
+
+protected
+	def encrypt_new_password
+		return if password.blank?
+		self.hashed_password = encrypt(password)
+	end
+
+	def password_required?
+		hashed_password.blank? || password.present?
+	end
+
+	def encrypt(string)
+		Digest::SHA1.hexdigest(string)
+	end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150907090828) do
+ActiveRecord::Schema.define(:version => 20150907133027) do
 
   create_table "games", :force => true do |t|
     t.string   "name"
@@ -22,6 +22,25 @@ ActiveRecord::Schema.define(:version => 20150907090828) do
     t.string   "platform"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "hashed_password"
+    t.string   "role"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
