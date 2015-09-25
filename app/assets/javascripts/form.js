212e60f6057fdee18906d761c72850b2e3f5b270
ChloeMoulin <chloe.moulin42@free.fr>
2015-09-25 11:18:04 +0200

    better validates JS

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,4 +1,4 @@
-function invalid_field(field, error, msg, span) {
+function invalid_field(field, error) {
    if(error)
       field.style.border = "1px solid #ea1515";
    else
@@ -10,13 +10,17 @@ function invalid_field(field, error, msg, span) {
 function check_username(field) {
 
   var regex = /^[a-zA-Z0-9_ éè]{2,15}$/i;
-  if(field.value.length < 2 || field.value.length > 15 || !regex.test(field.value)) {
-    invalid_field(field, true, "de 5 à 15 caractères", document.getElementById('username_error'));
-    document.getElementById('username_error').InnerHTML = "patate"
+  if(field.value.length < 2 || field.value.length > 15) {
+    invalid_field(field, true);
+    document.getElementById('username_error').innerHTML = "De 2 à 15 caractères"
     return false;
-  }
-  else {
+  } else if (!regex.test(field.value)) {
+      invalid_field(field, true);
+      document.getElementById('username_error').innerHTML = "Lettres maj, min, e accents et chiffres uniquement"
+      return false;
+  } else {
     invalid_field(field, false, "");
+    document.getElementById('username_error').innerHTML = ""
     return true;
    }
 }
@@ -24,12 +28,18 @@ function check_username(field) {
 function check_email(field) {
 
   var regex = /^[^@][\w.-]+@[\w.-]+[.][a-z]{2,4}$/i;
-  if(field.value.length < 5 || field.value.length > 30 || !regex.test(field.value)) {
+  if(field.value.length < 5 || field.value.length > 30) {
     invalid_field(field, true);
+    document.getElementById('email_error').innerHTML = "De 5 à 30 caractères"
     return false;
+  } else if (!regex.test(field.value)) {
+    invalid_field(field, true);
+    document.getElementById('email_error').innerHTML = "Adresse mail invalide"
+    return false;    
   }
   else {
     invalid_field(field, false);
+    document.getElementById('email_error').innerHTML = ""
     return true;
    }
 }
@@ -38,10 +48,12 @@ function check_password(field) {
 
   if(field.value.length < 10 || field.value.length > 50) {
     invalid_field(field, true);
+    document.getElementById('password_error').innerHTML = "De 10 à 50 caractères"
     return false;
   }
   else {
     invalid_field(field, false);
+    document.getElementById('password_error').innerHTML = ""
     return true;
    }
 }
@@ -50,10 +62,12 @@ function check_password_confirmation(field) {
 
   if(field.value != document.getElementById('user_password').value) {
     invalid_field(field, true);
+    document.getElementById('password_confirmation_error').innerHTML = "Les mdp doivent être identiques"
     return false;
   }
   else {
     invalid_field(field, false);
+    document.getElementById('password_confirmation_error').innerHTML = ""
     return true;
    }
 }
